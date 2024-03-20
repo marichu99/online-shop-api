998c1f7f05cbd5871816fc78ebe8695b123fabdc
marichu99 <martinmaati31@gmail.com>
2024-03-20 07:04:50 +0300

    inventory service intial bootstrap and order-service bug fix

--- order-service/src/main/java/com/product/order/entity/OrderItem.java
+++ order-service/src/main/java/com/product/order/entity/OrderItem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -18,7 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class OrderItem {
-    
+    @Id    
     @Column(name = "order_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
--- order-service/src/main/java/com/product/order/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/product/order/service/impl/OrderServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
 
 import com.product.order.entity.Order;
 import com.product.order.entity.OrderItem;
@@ -13,6 +14,7 @@ import com.product.order.model.OrderRequest;
 import com.product.order.repository.OrderRepository;
 import com.product.order.service.OrderService;
 
+@Service
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
 
