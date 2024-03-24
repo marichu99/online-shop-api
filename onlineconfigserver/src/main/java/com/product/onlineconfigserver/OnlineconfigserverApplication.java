0d5c78cd48ea39530eb6f3fb053eb4bacc364bde
marichu99 <martinmaati31@gmail.com>
2024-03-24 21:01:30 +0300

    add centralised config server

---
+++ onlineconfigserver/src/main/java/com/product/onlineconfigserver/OnlineconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.product.onlineconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineconfigserverApplication.class, args);
+	}
+
+}
