48df24d6d608f118bd50bfc946fa3b158b9eeb2a
marichu99 <martinmaati31@gmail.com>
2024-03-26 12:57:22 +0300

    zipkin distributed tracing

--- apiserver/src/main/java/com/product/apiserver/ApiserverApplication.java
+++ apiserver/src/main/java/com/product/apiserver/ApiserverApplication.java
@@ -2,8 +2,10 @@ package com.product.apiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiserverApplication {
 
 	public static void main(String[] args) {
--- payment/src/main/java/com/product/payment/service/impl/PaymentServiceImpl.java
+++ payment/src/main/java/com/product/payment/service/impl/PaymentServiceImpl.java
@@ -1,61 +1,76 @@
-package com.product.payment.service.impl;
+// package com.product.payment.service.impl;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.BeanUtils;
+// import org.springframework.core.ParameterizedTypeReference;
+// import org.springframework.http.HttpStatusCode;
+// import org.springframework.stereotype.Service;
+// import org.springframework.web.reactive.function.client.WebClient;
 
 
-import com.product.payment.entity.Payment;
-import com.product.payment.model.PaymentRequest;
-import com.product.payment.model.PaymentResponse;
-import com.product.payment.repository.ProductRepository;
-import com.product.payment.service.PaymentService;
 
-import lombok.extern.slf4j.Slf4j;
+// import com.product.payment.entity.Payment;
+// import com.product.payment.model.GenericResponse;
+// import com.product.payment.model.PaymentRequest;
+// import com.product.payment.model.PaymentResponse;
+// import com.product.payment.repository.ProductRepository;
+// import com.product.payment.service.PaymentService;
 
-@Service
-@Slf4j
-public class PaymentServiceImpl implements PaymentService {
-    private final ProductRepository productRepository;
+// import lombok.extern.slf4j.Slf4j;
 
-    public PaymentServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+// @Service
+// @Slf4j
+// public class PaymentServiceImpl implements PaymentService {
+//     private final ProductRepository productRepository;
+//     private final WebClient.Builder webClientBuilder;
 
-    @Override
-    public PaymentResponse makePayment(Payment payment) {
-        var madePayment = productRepository.save(payment);
-        return mapToPaymentResponse(madePayment);
-    }
+//     public PaymentServiceImpl(ProductRepository productRepository) {
+//         this.productRepository = productRepository;
+//     }
 
-    @Override
-    public List<PaymentResponse> getAllPayments() {
+//     @Override
+//     public Payment makePayment(PaymentRequest paymentRequest) {
 
-        return productRepository.findAll().stream().map(this::mapToPaymentResponse).toList();
-    }
+//         GenericResponse<?> response = webClientBuilder.build().get()
+//                 .uri("http://order-service/api/inventory/check",
+//                         uriBuilder -> uriBuilder                            .build())
+//                 .retrieve()
+//                 .onStatus(HttpStatusCode::isError, clientResponse -> handleError(clientResponse))
+//                 .bodyToMono(new ParameterizedTypeReference<GenericResponse<?>>() {
+//                 })
+//                 .block();
+//         var madePayment = productRepository.save(payment);
+//         return mapToPaymentResponse(madePayment);
+//     }
 
-    @Override
-    public PaymentResponse findPaymentById(int id) {
-        var payment =  productRepository.findById(id);
-        if (payment.isPresent()) {
-            return mapToPaymentResponse(payment.get());
-        }else{
-            log.info("Payment has not been found");
-        }
-        return null;
-    }
+//     @Override
+//     public List<PaymentResponse> getAllPayments() {
 
-    public PaymentResponse mapToPaymentResponse(Payment source) {
-        PaymentResponse payment = new PaymentResponse();
-        BeanUtils.copyProperties(source, payment);
-        return payment;
-    }
+//         return productRepository.findAll().stream().map(this::mapToPaymentResponse).toList();
+//     }
 
-    public Payment mapRequestToEntity(PaymentRequest request){
-        Payment entity=new Payment();
-        BeanUtils.copyProperties(request,entity);
-        return entity;
-    }
+//     @Override
+//     public PaymentResponse findPaymentById(int id) {
+//         var payment =  productRepository.findById(id);
+//         if (payment.isPresent()) {
+//             return mapToPaymentResponse(payment.get());
+//         }else{
+//             log.info("Payment has not been found");
+//         }
+//         return null;
+//     }
 
-}
+//     public PaymentResponse mapToPaymentResponse(Payment source) {
+//         PaymentResponse payment = new PaymentResponse();
+//         BeanUtils.copyProperties(source, payment);
+//         return payment;
+//     }
+
+//     public Payment mapRequestToEntity(PaymentRequest request){
+//         Payment entity=new Payment();
+//         BeanUtils.copyProperties(request,entity);
+//         return entity;
+//     }
+
+// }
